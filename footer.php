16f5f699822405000f24f71f7bc96410627412bc
ashish maurya <ashishmaurya@outlook.com>
2019-10-21 15:17:59 +0530

    Added footer

--- footer.php
+++ footer.php
@@ -2,12 +2,89 @@
 
 		</div><!-- .col-full -->
 	</div><!-- #content -->
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
+	<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
 
+<!-- Footer Links -->
+<div class="container-fluid text-center text-md-left">
 
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
+  <!-- Grid row -->
+  <div class="row">
+
+	<!-- Grid column -->
+	<div class="col-md-6 mt-md-0 mt-3">
+
+	  <!-- Content -->
+	  <h5 class="text-uppercase"><?php echo get_bloginfo( 'name' ); ?></h5>
+	  <p><?php echo get_bloginfo( 'description' ); ?></p>
+
+	</div>
+	<!-- Grid column -->
+
+	<hr class="clearfix w-100 d-md-none pb-3">
+
+	<!-- Grid column -->
+	<div class="col-md-3 mb-md-0 mb-3">
+
+	  <!-- Links -->
+	  <h5 class="text-uppercase">About Us</h5>
+
+	  <ul class="list-unstyled">
+		<li>
+		  <a href="#!">Contact</a>
+		</li>
+		<li>
+		  <a href="#!">Terms & Condition</a>
+		</li>
+		<li>
+		  <a href="#!">About</a>
+		</li>
+		<li>
+		  <a href="#!">Link 4</a>
+		</li>
+	  </ul>
+
+	</div>
+	<!-- Grid column -->
+
+	<!-- Grid column -->
+	<div class="col-md-3 mb-md-0 mb-3">
+
+	  <!-- Links -->
+	  <h5 class="text-uppercase">Shops</h5>
+
+	  <ul class="list-unstyled">
+		<li>
+		  <a href="#!">Products</a>
+		</li>
+		<li>
+		  <a href="#!">Category</a>
+		</li>
+		<li>
+		  <a href="#!">Link 3</a>
+		</li>
+		<li>
+		  <a href="#!">Link 4</a>
+		</li>
+	  </ul>
+
+	</div>
+	<!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">&copy; 2019 Copyright:
+  <a href="https://adesigner.in/"> Adesigner</a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
 </div><!-- #page -->
 
